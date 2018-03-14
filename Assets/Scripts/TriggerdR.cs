3fd1c10d5d81c323d5fe21e3740e705fc149f2df
Kevin Chu <kec073@UCSD.EDU>
2018-03-14 16:00:15 -0700

    necessary push for machine switch

--- Assets/Scripts/TriggerdR.cs
+++ Assets/Scripts/TriggerdR.cs
@@ -6,6 +6,7 @@ public class TriggerdR : MonoBehaviour {
     public static bool triggered = false;
     public static Vector3 rContact;
     bool contact_point = false;
+    Vector3 shoulder_pos;
 
     // OPTIONAL
     [SerializeField] private Transform m_RealHand;
@@ -15,6 +16,11 @@ public class TriggerdR : MonoBehaviour {
     [SerializeField] private float baseDistance = .2f;
     [SerializeField] private float criticalDist = 1f;
     [SerializeField] private float secondCriticalDist = 1.52f;
+
+    private void Start() {
+        shoulder_pos = m_Body.position;
+        shoulder_pos.x += 0.2f;
+    }
 
     void OnTriggerEnter(Collider other) {
         if (!contact_point) {
@@ -32,12 +38,14 @@ public class TriggerdR : MonoBehaviour {
         rContact = Vector3.zero;
     }
 
+    void Update() {
+        GogoAdjust();   // OPTIONAL
+    }
+
     // OPTIONAL: adjust the hand position
     void GogoAdjust() {
-        //float distance = Vector3.Magnitude(m_RealHand.position - m_Body.position);
-        OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch);
-        float distance = Vector3.Distance(m_RealHand.position, m_Body.position);
-        Debug.Log("hp " + m_RealHand.position + " mp " + m_Body.position + " " + distance);
+        float distance = Vector3.Magnitude(m_RealHand.position - m_Body.position);
+        Debug.Log(distance);
         transform.localPosition = new Vector3(
             0f,
             0f,
@@ -54,12 +62,9 @@ public class TriggerdR : MonoBehaviour {
         Debug.Log("firstdist " + firstDist);
         if (distance < secondCriticalDist)
             return firstDist;
-        Debug.Log("seconddist " + (firstDist + (distance - secondCriticalDist) * 40f));
-        return (firstDist + (distance - secondCriticalDist) * 40f);
-    }
 
-    void Update() {
-        GogoAdjust();   // OPTIONAL
+        Debug.Log("secondDist  " + (firstDist + (distance - secondCriticalDist) * 40f));
+        return firstDist + (distance - secondCriticalDist) * 40f;
     }
 
 }
